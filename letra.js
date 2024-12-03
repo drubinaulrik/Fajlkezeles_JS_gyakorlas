866f445c80a323eec11c682c039b29ab679a11e9
drubinaulrik <98951576+drubinaulrik@users.noreply.github.com>
2024-12-03 11:15:53 +0100

    Órai_feladat - 2024.12.03

---
+++ letra.js
@@ -0,0 +1,51 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+// import randomInt from 'randomInt'
+function randomInt(min, max) 
+{ 
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const filePath = path.join(__dirname, 'dobasok.txt')
+
+let content
+try{
+    content = fs.readFileSync(filePath, 'utf-8')
+} catch (err){
+    console.log(err)
+}
+console.log('1. feladat')
+console.log('\tAdatok beolvasása')
+
+console.log('2. feladat')
+let arr = content.split(', ').map(Number)
+let sum = 0
+let backward = 0
+for (let i = 0 ; i < arr.length ; i++){
+    arr[i] += sum
+    if (arr[i] % 10 == 0){
+        arr[i] -= 3
+        backward++
+    }
+    sum = arr[i]
+} 
+console.log(arr.join(' '))
+
+console.log('3. feladat')
+console.log(`A játéksorán ${backward} alkalommal lépett létrára.`)
+console.log('4. feladat')
+console.log(arr[arr.length - 1] >= 45 ? 'A játékot befejezte.' : 'A játékot abbahagyta')
+console.log('5. feladat')
+content += '\n'
+arr = []
+for (let i = 0; i < 18; i++){
+    arr.push(randomInt(1, 7))
+}
+content += arr.join(', ')
+try{
+    fs.writeFileSync(filePath, content)
+} catch(err){
+    console.log(err)
+}
